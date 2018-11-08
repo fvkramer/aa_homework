5deda140845f1f47d8ba4a486b8aceea2af253b3
FilippKramer <krame490@gmail.com>
2018-11-07 23:25:37 -0800

    N-Query Start

--- W3D3/Person/app/models/house.rb
+++ W3D3/Person/app/models/house.rb
@@ -1,7 +1,7 @@
 class House < ApplicationRecord
   validates :address, presence: true
 
-  has_many :residents,
+  has_many :people,
     primary_key: :id,
     foreign_key: :house_id,
     class_name: :Person
