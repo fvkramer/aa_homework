5878df08e2da2e5c95e8969d204cb89757ab2fcf
FilippKramer <krame490@gmail.com>
2018-11-24 11:50:20 -0800

    Finish canvas work

--- W6D1/canvas_practice/practice.js
+++ W6D1/canvas_practice/practice.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", function(){
+  const mycanvas = document.getElementById("mycanvas");
+  mycanvas.width = 500;
+  mycanvas.height = 500;
 
+  const ctx = mycanvas.getContext('2d');
+  ctx.fillStyle = 'red';
+  ctx.fillRect(0,0,500,500);
+
+  ctx.beginPath();
+  ctx.strokeStyle = 'blue';
+  ctx.arc(50, 50, 30, 0, 360);
+  ctx.lineWidth = 5;
+  ctx.stroke();
+  ctx.fillStyle = 'blue';
+  ctx.fill();
 });
