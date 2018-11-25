530828d10ac0ff12b9f76600f8df6721c638a935
FilippKramer <krame490@gmail.com>
2018-11-24 16:43:36 -0800

    Do W6D4 hwk

---
+++ W6D4/skeleton/script.js
@@ -0,0 +1,67 @@
+document.addEventListener("DOMContentLoaded", () => {
+  // toggling restaurants
+
+  const toggleLi = (e) => {
+    const li = e.target;
+    if (li.className === "visited") {
+      li.className = "";
+    } else {
+      li.className = "visited";
+    }
+  };
+
+  document.querySelectorAll("#restaurants li").forEach((li) => {
+    li.addEventListener("click", toggleLi);
+  });
+
+
+  const submit = document.getElementByClass('.favorite-submit');
+  submit.addEventListener('click', e => {
+    e.preventDefault();
+
+    const restaurantNameFav = document.getElementByClass('.favorite-input');
+    const restaurant = restaurantNameFav.value;
+    restaurantNameFav.value = "";
+
+    const favList = document.getElementById("sf-places");
+    const li = document.createElement("li");
+    li.textContent = restaurant
+
+    favList.appendChild(li);
+  });
+  // adding SF places as list items
+
+  // --- your code here!
+  const showPhotoForm = (e) => {
+    const photoFormDiv = document.querySelector(".photo-form-container");
+    if (photoFormDiv.className === "photo-form-container") {
+      photoFormDiv.className = "photo-form-container hidden";
+    } else {
+      photoFormDiv.className = "photo-form-container"
+    }
+  };
+
+  const photoFormShowButon = document.querySelector(".photo-show-button");
+  photoFormShowButton.addEventListener("click", showPhotoForm);
+
+  const handlePhotoSubmit = (e) => {
+    e.preventDefault();
+
+    const photoUrlInput = document.querySelector(".photo-url-input");
+    const photoUrl = photoUrlInput.value;
+    photoUrlInput.value = "";
+
+    const newImg = document.createElement("img");
+    newImg.src = photoUrl;
+
+    const newPhotoLi = document.createElement("li");
+    newPhotoLi.appendChild(newImg);
+
+    const dogPhotosList = document.querySelector(".dog-photos");
+    dogPhotosList.appendChild(newPhotoLi);
+  };
+
+  const photoSubmitButton = document.querySelector(".photo-url-submit");
+  photoSubmmitButton.addEventListener("click", handlePhotoSubmit);
+
+});
